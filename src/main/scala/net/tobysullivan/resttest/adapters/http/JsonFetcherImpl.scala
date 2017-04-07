c02d15e407f8ff8571ea18761671b6c7aa534e26
Toby Sullivan <tobyjsullivan@gmail.com>
2017-04-07 13:58:44 -0700

    Make HTTP timeouts configurable

--- src/main/scala/net/tobysullivan/resttest/adapters/http/JsonFetcherImpl.scala
+++ src/main/scala/net/tobysullivan/resttest/adapters/http/JsonFetcherImpl.scala
@@ -2,14 +2,22 @@ package net.tobysullivan.resttest.adapters.http
 
 import scalaj.http._
 import spray.json._
+import com.typesafe.config.ConfigFactory
 
 import scala.util.Try
 
 object JsonFetcherImpl extends JsonFetcher {
+  private val conf = ConfigFactory.load()
+
+  private val connTimeout: Int = conf.getInt("http.timeout.connect")
+  private val readTimeout: Int = conf.getInt("http.timeout.read")
+
   override def fetchSync(url: String): Try[Option[JsValue]] = Try {
     Http(url)
       .option(HttpOptions.method("GET"))
       .option(HttpOptions.followRedirects(true))
+      .option(HttpOptions.connTimeout(connTimeout))
+      .option(HttpOptions.readTimeout(readTimeout))
       .asString match {
       case resp @ HttpResponse(body, _, _) if resp.is2xx => Some(body.parseJson)
       case HttpResponse(_, 404, _) => None
