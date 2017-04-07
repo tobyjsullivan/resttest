8a90cc01dfda3affdd29381db60dc3da4f300177
Toby Sullivan <tobyjsullivan@gmail.com>
2017-04-07 14:25:15 -0700

    Acknowledge errors

--- src/main/scala/net/tobysullivan/resttest/App.scala
+++ src/main/scala/net/tobysullivan/resttest/App.scala
@@ -5,6 +5,8 @@ import net.tobysullivan.resttest.adapters.http.{JsonFetcher, JsonFetcherImpl}
 import net.tobysullivan.resttest.cli.DisplayFormat
 import net.tobysullivan.resttest.models.Statement
 
+import scala.util.{Failure, Success, Try}
+
 object App {
   def main(args: Array[String]) {
     // Initialize the adapters
@@ -12,9 +14,17 @@ object App {
     val bench = new BenchAdapter(fetcher)
 
     // Get a Statement object from the Bench adapter
-    val stmt: Statement = Statement(bench.allTransactions())
-
-    // Format the statement and print to screen
-    println(DisplayFormat.format(stmt))
+    Try {
+      Statement(bench.allTransactions())
+    } match {
+      case Success(stmt) =>
+        // Format the statement and print to screen
+        println(DisplayFormat.format(stmt))
+      case Failure(e) =>
+        println("Uh oh! There was an unexpected error. Maybe a configuration issue?")
+        println("ERROR " + e.toString)
+        e.printStackTrace()
+        System.exit(1)
+    }
   }
 }
